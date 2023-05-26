98b8a35b20bf1371032ce0ce9d66d4b9e597a3bc
Julibeto84 <you@example.com>
2023-05-26 10:43:14 -0700

    sort.h

--- sort.h
+++ sort.h
@@ -1,7 +1,5 @@
-#ifndef SORT_H
-#define SORT_H
-
-
+#ifndef __SORT__
+#define __SORT__
 #include <stdlib.h>
 
 /**
@@ -16,13 +14,24 @@ typedef struct listint_s
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
-} listin_t;
+} listint_t;
+
+/*======================================================================*/
+/*=================         Printing Algoritms        ==================*/
+/*======================================================================*/
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+
+/*======================================================================*/
+/*=================          sorting Algoritms        ==================*/
+/*======================================================================*/
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
-#endif
+#endif /* __SORT__ */
